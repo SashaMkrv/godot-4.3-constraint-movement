74d84284e6513231ffe1fd07174772a2e8b004ca
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-11-18 19:38:53 -0500

    add color exports to circle markers

--- example-motion/scripts/obstacle-nodes/circle-marker.gd
+++ example-motion/scripts/obstacle-nodes/circle-marker.gd
@@ -2,6 +2,16 @@
 extends Node2D
 class_name CircleMarker
 
+@export_color_no_alpha var fillColor:= Color.GREEN:
+	set(value):
+		fillColor = value
+		queue_redraw()
+		
+@export var fill: bool = true:
+	set(value):
+		fill = value
+		queue_redraw()
+
 signal radius_changed(newValue: int)
 
 @export_range(1, 10000) var radius: int = 100:
@@ -10,10 +20,14 @@ signal radius_changed(newValue: int)
 		radius_changed.emit(radius)
 		queue_redraw()
 
+
 func _draw() -> void:
 	draw_circle(
 		Vector2i.ZERO,
 		radius,
-		Color.GREEN,
-		true
+		fillColor,
+		fill
 	)
+
+func _ready() -> void:
+	queue_redraw()
