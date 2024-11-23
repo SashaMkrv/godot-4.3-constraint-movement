01b1c3f06948f48703155ad7f285ac1f44b13308
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-11-23 09:59:19 -0500

    pull out circle math into static functions

---
+++ example-motion/scripts/math/circle-geometry.gd
@@ -0,0 +1,111 @@
+extends RefCounted
+class_name CircleGeometry
+
+func tangentVectors(
+	point: Vector2,
+	circleOrigin: Vector2,
+	circleRadius: float
+) -> PackedVector2Array:
+	# I miss tuple returns.
+	var hypotnuse = (point - circleOrigin).length()
+	var opposite = circleRadius
+	var theta = asin(opposite/hypotnuse)
+	
+	var baseVector = (circleOrigin - point)
+	var upper = baseVector.rotated(theta)
+	var lower = baseVector.rotated(-theta)
+	return [upper.normalized(), lower.normalized()]
+
+# We're assuming there ARE intersection here.
+# Though I think this is just NaNs if not.
+func circleIntersections(
+	origin1: Vector2,
+	radius1: float,
+	origin2: Vector2,
+	radius2: float
+) -> PackedVector2Array:
+	var originDistance = (origin2 - origin1).length()
+	
+	var baseLength1 = (
+		pow(radius1, 2.0) - pow(radius2, 2.0) + pow(originDistance, 2.0)
+	)/(
+		2 * (originDistance)
+	)
+	
+	var height = sqrt(
+		pow(radius1, 2.0) - pow(baseLength1, 2.0)
+	)
+	
+	# multiple unit vector by traingle base length
+	var lineOfIntersectionCenter = (
+		origin2 - origin1
+	).normalized() * baseLength1 + origin1
+	
+	var unitBaseDirection = (
+		origin2 - origin1
+	).normalized()
+	var perpindicularUpper = Vector2(
+		unitBaseDirection.y,
+		-unitBaseDirection.x
+	) * height
+	var perpindicularLower = Vector2(
+		-unitBaseDirection.y,
+		unitBaseDirection.x
+	) * height
+	
+	
+	var upper = perpindicularUpper + lineOfIntersectionCenter
+	var lower = perpindicularLower + lineOfIntersectionCenter
+	return [upper, lower]
+
+func lineIntersectsCircle(
+	lineFrom: Vector2,
+	lineTo: Vector2,
+	circleOrigin: Vector2,
+	circleRadius: float
+) -> bool:
+	# the segment mentioned is the point
+	var closestPoint = Geometry2D.get_closest_point_to_segment(
+		circleOrigin,
+		lineFrom,
+		lineTo
+	)
+	var distanceFromCircleOrigin = (closestPoint - circleOrigin).length()
+	return (distanceFromCircleOrigin < circleRadius)
+
+func lineIntersectsCircleVeryExtra(
+	lineFrom: Vector2,
+	lineTo: Vector2,
+	circleOrigin: Vector2,
+	circleRadius: float
+) -> bool:
+	if Geometry2D.is_point_in_circle(
+		lineFrom,
+		circleOrigin,
+		circleRadius
+	) or Geometry2D.is_point_in_circle(
+		lineTo,
+		circleOrigin,
+		circleRadius
+	):
+		return true
+	
+	var closestPoint = Geometry2D.get_closest_point_to_segment(
+		circleOrigin,
+		lineFrom,
+		lineTo
+	)
+	
+	# I don't believe this is wrong per se,
+	# but i don't know why i did that second check.
+	# did i think the closest point wasn't always on the segment?
+	var distanceFromCircleOrigin = (closestPoint - circleOrigin).length()
+	var distanceFromLineEnd = (closestPoint - lineTo).length()
+	var segmentLength = (lineFrom - lineTo).length()
+	
+	return (
+		distanceFromCircleOrigin < circleRadius
+	) and (
+		distanceFromLineEnd < segmentLength
+	)
+	return false
